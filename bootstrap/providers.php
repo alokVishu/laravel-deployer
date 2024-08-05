627c9af2300dcf582b98f84b76f16f34df1511a4
alokVishu <alok@themeselection.com>
2024-08-05 18:14:27 +0530

    wip: bootstrap

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,10 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
+    App\Providers\ConfigProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\DashboardPanelProvider::class,
+    App\Providers\IconProvider::class,
+    App\Providers\NewsLetterServiceProvider::class,
 ];
